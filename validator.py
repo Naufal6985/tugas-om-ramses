48803028039ed78fed51804d6b2e9dc48bb0fdf8
Naufal6985 <novritaputri65@gmail.com>
2026-09-20 08:56:55 +0700

    apdet

--- validator.py
+++ validator.py
@@ -1,21 +1,22 @@
 nama_agen = input("Masukan nama agen: ")
-id_agen = int(input("Masukan ID agen: "))
+id_agen = input("Masukan ID agen: ")
 kata_sandi = input("Masukan kata sandi: ")
 
-def validate_access(name, id_number, passcode):
+def validate_access(name, id_agen, passcode):
+    output = []
     if name == "":
-        return False, "nama tidak boleh kosong"
-
-    if not (1000 <= id_number <= 9999):
-        return False, "ID harus berisi 4 digit"
-    if id_number =="":
-        return False, "nama tidak boleh ksong"
-
+        output.append("Nama tidak boleh kosong")
+    if id_agen == "":
+        output.append("ID tidak boleh kosong")
+    else:
+        id_agen = int(id_agen)
+        if not (1000 <= id_agen <= 9999):
+            output.append("ID harus berisi 4 digit")
     if len(passcode) < 8:
-        return False, "password harus berisi 8 digit"
-
+        output.append("Password harus minimal 8 karakter")
     if "!" not in passcode and "?" not in passcode:
-        return False,"Password harus berisikan karakter ! ?"
+        output.append("Password harus mengandung ! atau ?")
+    if output:
+        return False, "\n".join(output)
     return True, "Valid"
-validate_access(nama_agen, id_agen, kata_sandi)
-print(validate_access(nama_agen, id_agen, kata_sandi))
+print(validate_access(nama_agen, id_agen, kata_sandi)[1])
